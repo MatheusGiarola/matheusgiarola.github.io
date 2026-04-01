29da17d484070bcf5a06e2dbe416c3529692ba63
Matheus Giarola <matheusgiarola@usp.br>
2026-04-01 17:01:14 -0300

    First Commit
    Personalize Astro Paper framework

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://matheusgiarola.github.io", // replace this with your deployed domain
+  author: "Matheus Giarola",
+  profile: "https://matheusgiarola.github.io/",
+  desc: "Um site pessoal destinado a compartilhar minhas experiências em algumas disciplinas da graduação",
+  title: "Matheus Giarola",
+  ogImage: "website-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
@@ -12,12 +12,12 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
-    text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    enabled: false,
+    text: "",
+    url: ""
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "pt-BR", // html lang code. Set this empty and default will be "en"
+  timezone: "America/Sao_Paulo", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
